6dfe915b7c42b61343537c668fb02e7897d0378d
snera <xatnys@gmail.com>
2013-04-03 15:59:28 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+#global
+gem 'rails', '4.0.0.beta1'
+gem 'bootstrap-sass', '2.3.0.1'
+gem 'bcrypt-ruby', '3.0.1'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.0.1'
+
+group :test do
+  gem 'selenium-webdriver', '2.0'
+  gem 'capybara', '2.1.0.beta1'
+  gem 'factory_girl_rails', '4.2.0'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 4.0.0.beta1'
+  gem 'coffee-rails', '~> 4.0.0.beta1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+#for heroku
+group :production do
+  gem 'pg', '0.14.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Formify::Application.config.secret_key_base = '1520294851d9ac2998c4182b23a9eb504267188fb9adf1b9515c4060537eba837c43e7e60f13649e15305d421961cc5464663ddc58444d1fa6089f3bbea05e8c'
