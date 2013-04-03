79ef5e0d1224f7c284d52f7faaeb4631eaee208c
snera <xatnys@gmail.com>
2013-04-03 17:57:39 -0400

    Satisfy initial static#home tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Formify::Application.routes.draw do
-  get "static/home"
+  # get "static/home"
+
+  root to: 'static#home', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
