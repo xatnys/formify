503f4f7e69ebf795ee8923aad347281b7ba4dda0
snera <xatnys@gmail.com>
2013-04-04 16:26:38 -0400

    Update routes, remove old test file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Formify::Application.routes.draw do
   # get "static/home"
 
   root to: 'static#home', via: 'get'
+  get '/register' => 'user#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/statics_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Statics" do
-  describe "GET /" do
-    it "shows static#home" do
- 			get(root_path).should render_template('static/home')
-    end
-  end
-end
