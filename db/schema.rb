1d5554cb423e947ac680d37819c0116636d2f1da
snera <xatnys@gmail.com>
2013-04-04 08:08:29 -0400

    Update schema [remove password / password confirmation]

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130404112923) do
+ActiveRecord::Schema.define(version: 20130404115035) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
-    t.string   "password_confirmation"
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "created_at"
