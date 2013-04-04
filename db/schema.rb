39926e71952d4add0446f04f4488bf35f2c884a7
snera <xatnys@gmail.com>
2013-04-04 07:34:28 -0400

    Update users schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130403221605) do
+ActiveRecord::Schema.define(version: 20130404112923) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20130403221605) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin"
+    t.string   "session_token"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true
 
 end
