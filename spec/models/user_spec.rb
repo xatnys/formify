a61d4acfe39d02cad6e7c41b371b31334a5f78e2
snera <xatnys@gmail.com>
2013-04-13 16:34:16 -0400

    Modify User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,38 +2,45 @@ require 'spec_helper'
 
 describe User do
   
-  let(:blank_user) { User.new }
+	let(:blank_user) { User.new }
 	let(:user) { FactoryGirl.create(:user) }
   
-  describe "email validation" do
-	  it { should validate_presence_of(:email) }
-	  let(:bad_email) { %w[@cOM foo b,@r fizz@buh..com bad@mail+addr.com not@so_fast.com be@punctual. me_at_nospam.net bah ] }
-	  it "fails on invalid emails" do
-	  	bad_email.each do |addr|
-	  		user.email = addr
-	  		expect(user).to be_invalid
-	  	end
-	  end
-
-	  let(:mixed_case) { "mIxED@EMAIL.net" }
-	  it "stores email as lowercase" do
-	  	user.email = mixed_case
-	  	user.save!
-	  	expect(user.email).to eq(mixed_case.downcase)
-	  end
+	describe 'username validation' do
+		it { should_validate_presence_of(:username) }
 	end
+
+
+	# keep email as a possible option
+
+	# describe "email validation" do
+	#   it { should validate_presence_of(:email) }
+	#   let(:bad_email) { %w[@cOM foo b,@r fizz@buh..com bad@mail+addr.com not@so_fast.com be@punctual. me_at_nospam.net bah ] }
+	#   it "fails on invalid emails" do
+	#   	bad_email.each do |addr|
+	#   		user.email = addr
+	#   		expect(user).to be_invalid
+	#   	end
+	#   end
+
+	#   let(:mixed_case) { "mIxED@EMAIL.net" }
+	#   it "stores email as lowercase" do
+	#   	user.email = mixed_case
+	#   	user.save!
+	#   	expect(user.email).to eq(mixed_case.downcase)
+	#   end
+	# end
 
 	describe "name validation" do
   	it { should validate_presence_of(:first_name) }
   	it { should validate_presence_of(:last_name) }
 	end
 
+	# from has_secure_password helper
 	describe "password validation" do
 		
 		it { should validate_presence_of(:password) }
   	it { should validate_presence_of(:password_confirmation) }
-
-  	# from has_secure_password helper
+  	
   	it { should validate_presence_of(:password_digest) }
   	specify { expect(user).to respond_to(:authenticate) }
 
@@ -51,4 +58,13 @@ describe User do
   	expect(FactoryGirl.create(:admin)).to be_admin
   end
 
+  describe "reponds to a privilege bitfields" do
+  	it { should have_a_bitfield :priv_manage_ops }
+  	it { should have_a_bitfield :priv_add_cust_email }
+  	it { should have_a_bitfield :priv_add_cust_phone }
+  	it { should have_a_bitfield :priv_add_cust_kiosk }
+  	it { should have_a_bitfield :priv_view_cust }
+  	it [ should have_a_bitfield :priv_edit_cust ]
+  	it { should have_a_bitfield :priv_search_cust }
+  end
 end
